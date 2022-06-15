646ee59015e44ced67458757f29787eb38d9780e
Denis <vsekrasiva@gmail.com>
2022-06-15 13:00:54 +0300

    Ex16: Запрос данных другого пользователя

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -33,4 +33,21 @@ class TestUserGet(BaseCase):
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
 
+    def test_get_user_auth_other_id(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = MyRequests.post("/user/login", data=data)
 
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+        response2 = MyRequests.get(f"/user/{29790}",
+                                 headers ={"x-csrf-token": token},
+                                 cookies ={"auth_sid": auth_sid}
+                                 )
+        expected_fields = ["email", "firstName", "lastName"]
+        print(response2.text)
+        Assertions.assert_json_has_not_keys(response2, expected_fields)
